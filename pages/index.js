f736f5f222416b10a42f06e3b4db29631a0b9f6a
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2022-04-12 00:07:20 +0200

    Initialising project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import * as Unicons from '@iconscout/react-unicons';
+
+export default function Home() {
+  return (
+    <div>
+      <Unicons.Uil/>
+    </div>
+  )
+}
